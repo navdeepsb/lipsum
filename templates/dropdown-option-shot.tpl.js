2dac92b77b7e64dd210d7cb9188277fb0622c579
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2017-11-05 14:04:58 -0500

    Added one more shot

--- templates/dropdown-option-shot.tpl.js
+++ templates/dropdown-option-shot.tpl.js
@@ -1,4 +1,4 @@
-var _markup = "<option value='${ _key }' data-val='${ _key }'>${ parentXp } by @${ owner.split( '@' )[ 1 ] }, ${ createdOn }</option>";
+var _markup = "<option value='${ _key }' data-val='${ _key }'>${ caption } by @${ owner.split( '@' )[ 1 ] } in ${ parentXp }</option>";
 
 
 // Register this template:
